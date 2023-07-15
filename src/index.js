b6b9d601c95be0d98bec7c6d28599669b760203a
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-15 23:51:51 +0300

    Bloking (synchronous) way and Non-bloking (asynchronous) way

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+/*
+//Bloking, synchronous way
 console.log(fs.readFileSync("txt/input.txt", "utf-8"));
 
 const text = `This is what we know about avocade: ${fs.readFileSync(
@@ -8,5 +10,13 @@ const text = `This is what we know about avocade: ${fs.readFileSync(
 )}.\n Created in ${Date.now()}.`;
 
 console.log(text);
-
-fs.writeFileSync("txt/output.txt", text);
+*/
+fs.readFile("txt/start.txt", "utf-8", (err, file) => {
+  fs.readFile(`txt/${file}.txt`, "utf-8", (err, text1) => {
+    fs.readFile("txt/append.txt", "utf-8", (err, text2) => {
+      fs.writeFile("txt/final.txt", `${text1} \n ${text2}`, "utf-8", (err) => {
+        console.log("File has been written");
+      });
+    });
+  });
+});
