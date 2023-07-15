f25cddbd6607b84d938dfeb7f76be328b18e9401
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-16 00:18:04 +0300

    Creating a web server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+/*
+//Using file system package
 const fs = require("fs");
 
-/*
 //Bloking, synchronous way
 console.log(fs.readFileSync("txt/input.txt", "utf-8"));
 
@@ -10,7 +11,7 @@ const text = `This is what we know about avocade: ${fs.readFileSync(
 )}.\n Created in ${Date.now()}.`;
 
 console.log(text);
-*/
+
 fs.readFile("txt/start.txt", "utf-8", (err, file) => {
   fs.readFile(`txt/${file}.txt`, "utf-8", (err, text1) => {
     fs.readFile("txt/append.txt", "utf-8", (err, text2) => {
@@ -19,4 +20,17 @@ fs.readFile("txt/start.txt", "utf-8", (err, file) => {
       });
     });
   });
+});
+
+*/
+
+//Using http module for server
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
 });
