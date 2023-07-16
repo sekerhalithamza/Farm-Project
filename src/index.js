aa08ff8b29a99fac3fd01c002134604a11c44529
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-16 20:09:15 +0300

    Routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /*
+
 //Using file system package
 const fs = require("fs");
 
@@ -24,6 +25,8 @@ fs.readFile("txt/start.txt", "utf-8", (err, file) => {
 
 */
 
+/*
+
 //Using http module for server
 const http = require("http");
 
@@ -33,4 +36,27 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
+});
+
+*/
+
+//Routing pages for urls
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const path = req.url;
+
+  if (path === "/" || path === "/overview") res.end("The overview");
+  else if (path === "/product") res.end("The product");
+  else
+    res.writeHead("404", {
+      "Content-type": "text:html",
+      "my-own-header": "hello-world",
+    }),
+      res.end("<h1>Page not found</h1>");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening from server");
 });
