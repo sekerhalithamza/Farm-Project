6f0942428ce7155863da8349c8fd9e08baea7aea
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-17 10:12:37 +0300

    HTML TEMPLATING: Filling  Templates

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const text = `This is what we know about avocade: ${fs.readFileSync(
 
 console.log(text);
 
+// Non-blocking, asynchronous way
+
 fs.readFile("txt/start.txt", "utf-8", (err, file) => {
   fs.readFile(`txt/${file}.txt`, "utf-8", (err, text1) => {
     fs.readFile("txt/append.txt", "utf-8", (err, text2) => {
@@ -111,6 +113,8 @@ server.listen(8000, "127.0.0.1");
 
 */
 
+/*
+
 //HTML Templating: Building the templates
 const http = require("http");
 const url = require("url");
@@ -126,6 +130,56 @@ const server = http.createServer((req, res) => {
 
   if (path === "/overview" || path === "/") {
     res.end("This is the overview");
+  } else if (path === "/product") res.end("This is the product");
+  else if (path === "/api") {
+    res.writeHead("200", {
+      "Content-type": "application/json",
+    });
+    res.end(apiData);
+  } else
+    res.writeHead("404", {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    }),
+      res.end("<h1>Page not found</h1>");
+});
+
+server.listen(8000, "127.0.0.1");
+
+*/
+
+//HTML Templating: Filling the templates
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) output = output.replace(/{%ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync("templates/template-overview.html", "utf-8");
+const tempProduct = fs.readFileSync("templates/template-product.html", "utf-8");
+const tempCard = fs.readFileSync("templates/template-card.html", "utf-8");
+
+const apiData = fs.readFileSync("dev-data/data.json", "utf-8");
+
+const parsedData = JSON.parse(apiData);
+
+const server = http.createServer((req, res) => {
+  const path = req.url;
+
+  if (path === "/overview" || path === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = parsedData.map((element) => replaceTemplate(tempCard, element)).join("");
+    const output = tempOverview.replace("{%PRODUCTS_CARDS%}", cardsHtml);
+    res.end(output);
   } else if (path === "/product") res.end("This is the product");
   else if (path === "/api") {
     res.writeHead("200", {
